e0e2d7a28632b1b7ad65403092c5ddc94f1ca36c
Michael Vo <zax@zaxu.xyz>
2023-08-27 00:38:02 +1000

    feat(navbar): link icons, use heroicons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,29 @@
 import { useAtom } from "jotai";
 import datePickedAtom from "~/atoms/datePickedAtom";
 import dayjs from "dayjs";
+import Link from "next/link";
+import { CalendarDaysIcon } from "@heroicons/react/24/solid";
+import { BellIcon } from "@heroicons/react/24/outline";
 
 const Navbar = () => {
   const [datePicked] = useAtom(datePickedAtom);
 
   return (
     <div className="flex justify-between bg-gray-100 px-6 py-2">
-      <img
-        className="h-10"
-        src="https://static.thenounproject.com/png/335121-200.png"
-      />
+      <Link href="/home">
+        <img
+          className="h-10"
+          src="https://static.thenounproject.com/png/335121-200.png"
+          alt="Home"
+        />
+      </Link>
       <div className="flex items-center gap-4">
         <p>{dayjs(datePicked).format("MMMM")}</p>
-        <img
-          className="h-7"
-          src="https://png.pngtree.com/png-clipart/20190614/original/pngtree-vector-calendar-icon-png-image_3782243.jpg"
-        />
-        <img
-          className="h-5"
-          src="https://cdn-icons-png.flaticon.com/512/3119/3119338.png"
-        />
+        <Link href="/calendar">
+          <CalendarDaysIcon className="h-7 w-7" />
+          <div className="sr-only">Calendar</div>
+        </Link>
+        <BellIcon className="h-6 w-6" />
       </div>
     </div>
   );
