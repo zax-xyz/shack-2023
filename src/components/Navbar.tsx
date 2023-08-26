549d8ca76058f3f88810ded564dc8a3910dd1438
Jasmine Tran <z5394841@ad.unsw.edu.au>
2023-08-27 07:38:48 +1000

    hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,26 @@ import datePickedAtom from "~/atoms/datePickedAtom";
 import dayjs from "dayjs";
 import logo from "~/assets/logo.png";
 import Link from "next/link";
-import { CalendarDaysIcon } from "@heroicons/react/24/solid";
+import { CalendarDaysIcon, HomeIcon } from "@heroicons/react/24/solid";
+import { UserIcon } from "@heroicons/react/24/solid";
 import NotifsPopover from "./NotifsPopover";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import React, { useState } from "react";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 const Navbar = () => {
   const [datePicked] = useAtom(datePickedAtom);
+  const { isOpen, onToggle, onClose } = useDisclosure()
 
   return (
     <div className="sticky top-0 rounded bg-gradient-to-r from-purple-200/50 to-violet-200/50 px-6 py-2 shadow backdrop-blur">
@@ -21,11 +35,45 @@ const Navbar = () => {
         </Link>
         <div className="flex items-center gap-4">
           <p>{dayjs(datePicked).format("MMMM")}</p>
-          <Link href="/calendar">
-            <CalendarDaysIcon className="h-7 w-7" />
-            <div className="sr-only">Calendar</div>
-          </Link>
           <NotifsPopover />
+        
+
+
+        <Popover placement="bottom-end" isOpen={isOpen} onClose={onClose}>
+        <PopoverTrigger>
+          <button className="flex items-center justify-center" onClick={onToggle}>
+            <Bars3Icon className="h-6 w-6" />
+          </button>
+        </PopoverTrigger>
+        <PopoverContent
+          bg="white"
+          color="black"
+          position="sticky"
+          tw="mx-auto max-w-xl min-w-48 rounded-md bg-white shadow outline-none focus-visible:(ring ring-violet-400/50)"
+        >
+          <PopoverArrow />
+          <PopoverBody className="pt-4 px-3">
+            <div className="mb-3" onClick={onClose} >
+              <Link href="/home" className="flex items-center gap-2">
+                <HomeIcon className="h-7 w-7" />
+                <div>Home</div>
+              </Link>
+            </div>
+            <div className="mb-3" onClick={onClose}>
+              <Link href="/calendar" className="flex items-center gap-2">
+                <CalendarDaysIcon className="h-7 w-7" />
+                <div>Calendar</div>
+              </Link>
+            </div>
+            <div className="mb-3" onClick={onClose}>
+              <Link href="/calendar" className="flex items-center gap-2">
+                <UserIcon className="h-7 w-7" />
+                <div>Logout</div>
+              </Link>
+            </div>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
         </div>
       </div>
     </div>
