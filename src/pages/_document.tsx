1f4ecbad62fc71a5ab676d19bf8b85992812afe7
Michael Vo <zax@zaxu.xyz>
2023-08-26 22:30:41 +1000

    feat: use twemoji

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,6 +43,11 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://unpkg.com/twemoji@latest/dist/twemoji.min.js"
+            crossOrigin="anonymous"
+          ></script>
         </Head>
         <body>
           <Main />
