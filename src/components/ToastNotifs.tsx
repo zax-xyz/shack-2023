c822ff4ce083058b0d3779b183997553cb0ca2c3
Michael Vo <zax@zaxu.xyz>
2023-08-27 02:17:33 +1000

    feat(toasts): transitions

--- src/components/ToastNotifs.tsx
+++ src/components/ToastNotifs.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import React, { Fragment } from "react";
 import toast from "react-hot-toast";
 import type { Toast as ToastObject } from "react-hot-toast";
 import type { VariantProps } from "@stitches/react";
 import tw, { styled } from "twin.macro";
+import Transition from "./Transition";
 
 const ButtonContainer = styled("div", tw`border-l border-gray-200`);
 
@@ -43,16 +45,25 @@ const ToastNotifs = ({
   description,
   type = "notification",
 }: Props) => (
-  <div>
-    <ToastContainer type={type}>
-      <div tw="flex-1 flex flex-col px-4 py-3">
-        <h1 tw="font-semibold">{title}</h1>
-        <p tw="text-sm">{description}</p>
-      </div>
-      <ButtonContainer>
-        <button
-          type="button"
-          tw="
+  <Transition
+    as={Fragment}
+    show={t.visible}
+    appear
+    enter={tw`duration-200 ease-out`}
+    enterFrom={tw`opacity-0 scale-95`}
+    leave={tw`duration-150 ease-in`}
+    leaveTo={tw`opacity-0 scale-95`}
+  >
+    <div>
+      <ToastContainer type={type}>
+        <div tw="flex-1 flex flex-col px-4 py-3">
+          <h1 tw="font-semibold">{title}</h1>
+          <p tw="text-sm">{description}</p>
+        </div>
+        <ButtonContainer>
+          <button
+            type="button"
+            tw="
             w-12 h-full py-3 px-2
             rounded-xl
             flex items-center justify-center
@@ -60,13 +71,14 @@ const ToastNotifs = ({
             hover:(text-purple-600 bg-slate-50)
             focus-visible:(outline-none ring-2 ring-purple-600)
           "
-          onClick={() => toast.dismiss(t.id)}
-        >
-          X
-        </button>
-      </ButtonContainer>
-    </ToastContainer>
-  </div>
+            onClick={() => toast.dismiss(t.id)}
+          >
+            X
+          </button>
+        </ButtonContainer>
+      </ToastContainer>
+    </div>
+  </Transition>
 );
 
 export default ToastNotifs;
