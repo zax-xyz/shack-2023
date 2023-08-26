5243b572470af911a7aecc207144881d391cd3b6
Jasmine Tran <z5394841@ad.unsw.edu.au>
2023-08-27 04:57:36 +1000

    styled unlogged page

--- src/components/UnloggedDay.tsx
+++ src/components/UnloggedDay.tsx
@@ -10,13 +10,13 @@ const UnloggedDay: React.FC<UnloggedDayProps> = ({ datePicked }) => {
 
   return (
     <div className="flex flex-col">
-      <div className="self-center my-8 font-bold text-3xl text-center">
-        You haven't logged for today
+      <div className="self-center mt-8 mb-3 text-3xl text-center text-indigo-500 mx-8">
+        You haven't logged for today :(
       </div>
 
       <Link className="self-center m-4" href={`/moodForm?datePicked=${datePicked}`}>
-        <button tw="rounded px-3 py-1.5 transition bg-white border border-violet-300 text-gray-900 hover:bg-violet-50">
-          Log Your Mood
+        <button tw="bg-gradient-to-r from-indigo-400/80 to-purple-400/80 rounded-full px-6 py-3 transition bg-white border border-violet-300 text-gray-900 hover:bg-violet-50">
+          <p className="text-2xl text-white font-semibold">Log Your Mood</p>
         </button>
       </Link>
     </div>
