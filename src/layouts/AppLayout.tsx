0aa5381fc110a19e79fd7de5e26f2e78c7985bfa
Michael Vo <zax@zaxu.xyz>
2023-08-27 01:07:05 +1000

    feat: fancy navbar

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,10 +2,10 @@ import { type ReactElement } from "react";
 import Navbar from "~/components/Navbar";
 
 const AppLayout = (page: ReactElement) => (
-  <div tw="min-h-screen bg-gray-50 p-4">
+  <div tw="min-h-screen bg-gray-50">
     <div tw="max-w-5xl mx-auto">
       <Navbar />
-      {page}
+      <div tw="p-4 pt-0">{page}</div>
     </div>
   </div>
 );
