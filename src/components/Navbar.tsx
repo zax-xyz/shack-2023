7fd7859b5bb445cc0032cf90b7fe8d39be701be7
Michael Vo <zax@zaxu.xyz>
2023-08-27 03:05:29 +1000

    fix(navbar): full width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,21 +9,23 @@ const Navbar = () => {
   const [datePicked] = useAtom(datePickedAtom);
 
   return (
-    <div className="sticky top-0 flex justify-between rounded bg-gradient-to-r from-purple-200/50 to-violet-200/50 px-6 py-2 shadow backdrop-blur">
-      <Link href="/home">
-        <img
-          className="h-10"
-          src="https://static.thenounproject.com/png/335121-200.png"
-          alt="Home"
-        />
-      </Link>
-      <div className="flex items-center gap-4">
-        <p>{dayjs(datePicked).format("MMMM")}</p>
-        <Link href="/calendar">
-          <CalendarDaysIcon className="h-7 w-7" />
-          <div className="sr-only">Calendar</div>
+    <div className="sticky top-0 rounded bg-gradient-to-r from-purple-200/50 to-violet-200/50 px-6 py-2 shadow backdrop-blur">
+      <div className="mx-auto flex max-w-5xl justify-between">
+        <Link href="/home">
+          <img
+            className="h-10"
+            src="https://static.thenounproject.com/png/335121-200.png"
+            alt="Home"
+          />
         </Link>
-        <BellIcon className="h-6 w-6" />
+        <div className="flex items-center gap-4">
+          <p>{dayjs(datePicked).format("MMMM")}</p>
+          <Link href="/calendar">
+            <CalendarDaysIcon className="h-7 w-7" />
+            <div className="sr-only">Calendar</div>
+          </Link>
+          <BellIcon className="h-6 w-6" />
+        </div>
       </div>
     </div>
   );
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -4,8 +4,8 @@ import { Toaster } from "react-hot-toast";
 
 const AppLayout = (page: ReactElement) => (
   <div tw="min-h-screen bg-gray-50">
+    <Navbar />
     <div tw="max-w-5xl mx-auto">
-      <Navbar />
       <div tw="p-4">{page}</div>
       <Toaster
         position="bottom-center"
